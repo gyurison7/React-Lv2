38de9ee8e2d9a0bee4980d191ee273b908dc0dc0
jonghwa4077 <ehdxka3@naver.com>
2023-07-01 09:40:44 +0900

    Feat: input 창 작성

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    const [input, setInput] = useState({ title: '', content: '' })
+
+    const inputChange = (event) => {
+        const { name, value } = event.target
+        setInput({ ...input, [name]: value })
+    }
+    return (
+        <div>
+            <div>
+                <h2>My Todo List</h2>
+                <input
+                    type='text'
+                    placeholder='제목'
+                    value={input.title}
+                    name='title'
+                    onChange={inputChange}
+                />
+                <input
+                    type='text'
+                    placeholder='내용'
+                    value={input.content}
+                    name='content'
+                    onChange={inputChange} />
+                    <button>추가하기</button>
+            </div>
+        </div>
+    )
 }
 
 export default Home
