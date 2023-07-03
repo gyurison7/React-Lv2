a533d69c3cebe0ab55ecc1cf7b0a0467ad0654bf
jonghwa4077 <ehdxka3@naver.com>
2023-07-03 19:36:22 +0900

    Header 컴포넌트 분리

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,14 +3,14 @@ import { useSelector } from 'react-redux'
 import InputForm from '../component/inputForm'
 import * as S from '../style/styledComponents'
 import List from '../component/list'
+import Header from '../component/Header'
 
 function Home() {
     const todos = useSelector((state) => state.todos.todos)
 
-
     return (
         <S.Layout>
-            <h2>My Todo List</h2>
+            <Header/>
             <InputForm todos={todos}/>
             <List todos={todos} />    
         </S.Layout>
---
+++ src/component/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+        <h2>My Todo List</h2>
+    </div>
+  )
+}
+
+export default Header
